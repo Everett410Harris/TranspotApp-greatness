477f015181a3afad5929cf7ab9ac7c6aadd8641b
Everett Harris <everettharr979@gmail.com>
2022-06-26 16:12:34 -0400

    removed vehicle

--- TransportationApp/Vehicles.cs
+++ TransportationApp/Vehicles.cs
@@ -1,104 +1,12 @@
-﻿using System.Data.SqlClient;
-using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace TransportationApp
 {
-    public partial class Vehicles : Form
+    internal class Vehicles
     {
-        public Vehicles()
-        {
-            InitializeComponent();
-        }
-        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Tech Louisville\Documents\transApp.mdf;Integrated Security=True;Connect Timeout=30");
-        private void SaveBtn_Click(object sender, EventArgs e)
-        {
-            if (LPlateTb.Text == "" || MakeCb.SelectedIndex == -1 || ModelTb.Text == "" || YearCb.SelectedIndex == -1 || EngTypeCb.SelectedIndex == -1 || ColorTb.Text == "" || MileageTb.Text == "" || TypeCb.SelectedIndex == -1 || BookedCb.SelectedIndex == -1)
-            {
-                MessageBox.Show("Missing Information");
-            }
-            else
-            {
-                try
-                {
-                    Con.Open();
-                    SqlCommand cmd = new SqlCommand("insert into VehicleTbl (VLp,VMake,VModel,VYear,VEngType,VColor,VMileage,VType,Booked) values(@VP,@VMa,@VMo,@VY,@VEng,@VCo,@VMi,@VTy,@VB)", Con);
-                    cmd.Parameters.AddWithValue("@VP", LPlateTb.Text);
-                    cmd.Parameters.AddWithValue("@VMa", MakeCb.SelectedItem.ToString());
-                    cmd.Parameters.AddWithValue("@VMo", ModelTb.Text);
-                    cmd.Parameters.AddWithValue("@VY", YearCb.SelectedItem.ToString());
-                    cmd.Parameters.AddWithValue("@VEng", EngTypeCb.SelectedItem.ToString());
-                    cmd.Parameters.AddWithValue("@VCo", ColorTb.Text);
-                    cmd.Parameters.AddWithValue("@VMi", MileageTb.Text);
-                    cmd.Parameters.AddWithValue("@VTy", TypeCb.SelectedItem.ToString());
-                    cmd.Parameters.AddWithValue("@VB", BookedCb.SelectedItem.ToString());
-                    cmd.ExecuteNonQuery();
-                    MessageBox.Show("Vehicle Recorded");
-                    Con.Close();
-
-                }
-                catch (Exception Ex)
-                {
-                    MessageBox.Show(Ex.Message);
-                }
-
-            }
-        }
-
-        private void panel4_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Vehicles_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox11_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox9_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label19_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label14_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label15_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label16_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label18_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label17_Click(object sender, EventArgs e)
-        {
-
-        }
-        
     }
 }
